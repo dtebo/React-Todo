02c02133945e9043ef7cb0d03c4080d21650e268
Darren Tebo <darrentebo83@outlook.com>
2020-07-08 19:09:13 -0400

    added todolist and todo components

---
+++ src/components/Todo.js
@@ -0,0 +1,7 @@
+import React from 'React';
+
+const Todo = props => {
+
+};
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from '../components/Todo';
+
+const TodoList = props => {
+    props.todos.map((todo) => {
+        return (
+            <Todo />
+        );
+    });
+};
+
+export default TodoList;
